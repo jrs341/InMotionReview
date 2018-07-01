14ece4391bc72c6eca8f7da7dc593ee59f18f7fe
Jason Sinn <jrs341@me.com>
2018-07-01 12:06:27 -0500

    refatored app.js slightly to handle empty ratings array left else statement for later

--- views/app.js
+++ views/app.js
@@ -1,6 +1,6 @@
 
 	renderMoviesUl = (data) => {
-		data.slice(0,3).map((movie, i) => {
+		data.slice(0,100).map((movie, i) => {
 			const node = document.createElement('UL')
 			node.setAttribute('class', movie.imdbId)
 			node.innerHTML = movie.title
@@ -18,12 +18,9 @@
 			ratings: data.ratings
 		}
 
-		for(var key in data) {
-			if (key == '_id' || key == 'imdbId' || key == 'title' ){
-			} else if (key == 'ratings') {
-				if (data[key].length > 0) {
-					renderRatingsUl(data[key], id)
-				}
+		for (var key in data) {
+			if (key == 'ratings') {
+				renderRatingsUl(data[key], id)
 			} else {
 			const li = document.createElement('LI')         
 			const textnode = document.createTextNode(data[key])
@@ -37,9 +34,13 @@
 		const ratingUl = document.createElement('UL')
 		const ratingUlId = 'rating' + id
 		ratingUl.setAttribute('class', ratingUlId)
-		ratingUl.innerHTML = 'Ratings'         
+		ratingUl.innerHTML = 'Ratings' 
+		if (data.length > 0){        
 		document.querySelector('ul.' + id).appendChild(ratingUl)
 		renderRatingsData(data, ratingUlId)
+		} else {
+
+		}
 	}
 
 	renderRatingsData = (data, id) => {
