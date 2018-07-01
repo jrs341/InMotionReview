e72d9ee006ca3a392010e820cdb0e64f932a42b5
Jason Sinn <jrs341@me.com>
2018-07-01 11:31:39 -0500

    added functions to render the ratings if any

--- views/app.js
+++ views/app.js
@@ -1,7 +1,6 @@
 
 	renderMoviesUl = (data) => {
 		data.slice(0,3).map((movie, i) => {
-			console.log(i)
 			const node = document.createElement('UL')
 			node.setAttribute('class', movie.imdbId)
 			document.querySelector('div.movieList').appendChild(node)
@@ -10,15 +9,39 @@
 	}
 
 	renderMovieData = (data) => {
-		console.log(data)
+		console.log('data', data)
 		for(var key in data) {
-			console.log('li')
+			if (key == '_id' || key == 'imdbId'){
+			} else if (key == 'ratings') {
+				if (data[key].length > 0) {
+					renderRatingsUl(data[key], data.imdbId)
+				}
+			} else {
 			const li = document.createElement('LI')         
 			const textnode = document.createTextNode(data[key])
 			li.appendChild(textnode)
 			document.querySelector('ul.' + data.imdbId).appendChild(li)
+			}
 		}
 	}
+
+	renderRatingsUl = (data, id) => {
+		const ratingUl = document.createElement('UL')
+		const ratingUlId = 'rating' + id
+		ratingUl.setAttribute('class', ratingUlId)         
+		document.querySelector('ul.' + id).appendChild(ratingUl)
+		renderRatingsData(data, ratingUlId)
+	}
+
+	renderRatingsData = (data, id) => {
+		data.map((ratings, i) => {
+			const li = document.createElement('LI')
+			const textnode = document.createTextNode(ratings.Source + ': ' + ratings.Value)
+			li.appendChild(textnode)
+			document.querySelector('ul.' + id).appendChild(li)
+		})
+	}
+
 $(document).ready(() => {
 
 })
