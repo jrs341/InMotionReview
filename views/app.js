f5719087cb5f9776580fbe5cf9158a54bb89a467
Jason Sinn <jrs341@me.com>
2018-07-01 11:48:08 -0500

    ordered the data

--- views/app.js
+++ views/app.js
@@ -3,24 +3,32 @@
 		data.slice(0,3).map((movie, i) => {
 			const node = document.createElement('UL')
 			node.setAttribute('class', movie.imdbId)
+			node.innerHTML = movie.title
 			document.querySelector('div.movieList').appendChild(node)
 			renderMovieData(movie)
 		})
 	}
 
 	renderMovieData = (data) => {
-		console.log('data', data)
+		const id = data.imdbId
+		data = {
+			genre: data.genre,
+			actors: data.actors,
+			year: data.year,
+			ratings: data.ratings
+		}
+
 		for(var key in data) {
-			if (key == '_id' || key == 'imdbId'){
+			if (key == '_id' || key == 'imdbId' || key == 'title' ){
 			} else if (key == 'ratings') {
 				if (data[key].length > 0) {
-					renderRatingsUl(data[key], data.imdbId)
+					renderRatingsUl(data[key], id)
 				}
 			} else {
 			const li = document.createElement('LI')         
 			const textnode = document.createTextNode(data[key])
 			li.appendChild(textnode)
-			document.querySelector('ul.' + data.imdbId).appendChild(li)
+			document.querySelector('ul.' + id).appendChild(li)
 			}
 		}
 	}
@@ -28,7 +36,8 @@
 	renderRatingsUl = (data, id) => {
 		const ratingUl = document.createElement('UL')
 		const ratingUlId = 'rating' + id
-		ratingUl.setAttribute('class', ratingUlId)         
+		ratingUl.setAttribute('class', ratingUlId)
+		ratingUl.innerHTML = 'Ratings'         
 		document.querySelector('ul.' + id).appendChild(ratingUl)
 		renderRatingsData(data, ratingUlId)
 	}
